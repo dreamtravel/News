2cfefd143898c27e3731b4c58729c36ff13d9d8a
user <user@userdeMac.local>
2015-09-04 15:16:06 +0800

    Initial Commit

---
+++ 新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  新闻
+//
+//  Created by user on 15/9/4.
+//  Copyright (c) 2015年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
